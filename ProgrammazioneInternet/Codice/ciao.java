22b3b9078ae9d8dd24308c3a811d84b5cd42f2ee
asdru22 <76167257+asdru22@users.noreply.github.com>
2023-01-15 20:18:32 +0100

    oddio come si java

--- ProgrammazioneInternet/Codice/ciao.java
+++ ProgrammazioneInternet/Codice/ciao.java
@@ -1,45 +1,21 @@
-/*class ciao {
-    public static void main(String[] args) {
-        // INTRO
-        System.out.println("ciao");
-        // Area triangolo
-        int base=4;
-        int altezza=5;
-        System.out.println(base*altezza/2);
-    }
-}*/
-
-/*import java.io.*;
-class ciao {
- public static void main(String[] args)
- throws IOException {
- int penultimo, ultimo, contatore;
- contatore = 0;
- ultimo = System.in.read();
- while (ultimo != '$') {
- penultimo = ultimo;
- ultimo = System.in.read();
- if (ultimo == penultimo)
- contatore = contatore + 1;
- }
- System.out.print("Numero di doppie: ");
- System.out.println(contatore);
- }
-}*/
-
-/*class ciao {
-    public static void main(String[] args) {       
-        int c = 5; // c tra 0 e 100
-       int z = c < 0 ? (c-1) : (c+1);
-    }
-}*/
-
-class ciao {
-    public ciao(String string) {
+import java.util.Scanner;
+class ciao{
+    public static void main(String[] args){
+        es1();
     }
 
-    public static void main(String[] args) {       
-        String b = args[0];
-        System.out.println("ciao" +b+".\tbbb");
+    static void es1(){
+        int iter = -1;
+        double tot = 0;
+        double temp;
+        Scanner in = new Scanner(System.in);
+
+        do {
+            temp = in.nextDouble();
+            tot+= temp;
+            iter += 1;
+        } while(temp!=0);
+        System.out.println(tot/iter);
     }
+
 }
---
+++ ProgrammazioneInternet/Codice/es_esame/es.java
@@ -0,0 +1,62 @@
+class es{
+    public static void main(String[] args){
+        //es1();
+        es2();
+    }
+    // Definire un metodo ricorsivo che prende in input un vettore di numeri interi e restituisce
+    //true se sono tutti uguali o false in caso contrario.
+
+    static void es2(){
+        int v[] = {1,1,1,1,0,1,1,1,1};
+        System.out.println(check(v,1));
+    }
+
+    static boolean check(int[] v,int i){
+        if(i==v.length) return true;
+        return (v[i]==v[0])? check(v,i+1) : false;
+    }
+
+    static void es1(){
+        int A[][] = matrix(6,6);
+        fill(A);
+        printmatrix(A);
+
+    }
+
+    static int[][] fill(int[][] mat){
+        for(int i = 1;i<mat.length;i++)
+            for(int j = 0;j<mat.length;j++){
+                mat[i][j] = sum(mat,i,j);
+            }
+            System.out.println();
+        return mat;
+    }
+    // i = righe, j = colonne
+    static int sum(int[][] A, int r, int c){
+        System.out.print(r+","+c+" ");
+        int tot = 0;
+        tot += A[r-1][c];
+        if(c-1>=0) tot += A[r-1][c-1];
+        if(c+1<A.length) tot += A[r-1][c+1];
+        return tot;
+    }
+
+    static int[][] matrix(int m,int n){
+        int[][] mat = new int [m][n];
+	    for(int i = 0;i<mat.length;i++)
+                mat[0][i] = random();
+	    return mat;
+    }
+
+    static int random(){
+        return (int)(Math.random()*10)%10;
+    }
+
+	static void printmatrix(int[][] mat){
+		System.out.println("-------------------------------");
+		for(int i = 0;i<mat.length;i++){
+			for(int j = 0;j<mat[0].length;j++) System.out.print(mat[i][j]+"\t");
+			System.out.println();
+			}
+		}
+}
